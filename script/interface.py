4baa3c46467b671e8049634296b6931a63f50664
repen <9keepa@gmail.com>
2021-03-14 16:18:10 +0500

    добавленны новые поля для контроллера

--- script/interface.py
+++ script/interface.py
@@ -6,9 +6,11 @@ class IRequest:
     url: str
     param: dict
     expiration_date: int
+    method: str
     wait: float = 1.1
     jscript: str = ""
 
 @dataclass
 class IPageResult:
     id: str
+    method: str
